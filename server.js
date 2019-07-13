7faf8ebdf762148e2274c5e14e8835a24522d790
taha <haqtaha@gmail.com>
2019-07-13 23:09:31 +0500

    Moved certs

--- server.js
+++ server.js
@@ -30,8 +30,8 @@ app.use('/', express.static(__dirname + '/build'))
 })*/
 
 var options = {
-  key: fs.readFileSync('../global.super.one/privkey.pem'),
-  cert: fs.readFileSync('../global.super.one/fullchain.pem')
+  key: fs.readFileSync('/etc/letsencrypt/live/global.super.one/privkey.pem'),
+  cert: fs.readFileSync('/etc/letsencrypt/live/global.super.one/fullchain.pem')
 };
 
 http.createServer(app).listen(8080);
